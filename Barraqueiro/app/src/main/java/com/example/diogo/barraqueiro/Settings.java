58be0012d76fc3cdbdce5fa8c95b687887d0a29b
Diogo1995 <diogofilipe_10-1@hotmail.com>
2018-03-20 13:26:07 +0000

    settings layout v2
    settings layout ja com 2 opções

--- Barraqueiro/app/src/main/java/com/example/diogo/barraqueiro/Settings.java
+++ Barraqueiro/app/src/main/java/com/example/diogo/barraqueiro/Settings.java
@@ -3,6 +3,7 @@ package com.example.diogo.barraqueiro;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
 
@@ -11,5 +12,16 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Ver o estado da switch
+        Switch darkModeSwitch = (Switch) findViewById(R.id.darkModeSwitch);
+        Boolean darkModeSwitchState = darkModeSwitch.isChecked();
+
+        Switch unitsSwitch = (Switch) findViewById(R.id.unitsSwitch);
+        Boolean unitsSwitchState = unitsSwitch.isChecked();
+        if (unitsSwitchState) unitsSwitch.setText("@string/USCUnits"); // displayed text of the Switch whenever it is in checked or on state
+        else unitsSwitch.setText("@string/SIBUnits"); // displayed text of the Switch whenever it is in unchecked i.e. off state
+
+        //TODO gravar posição da switch escolhida!!
     }
 }
