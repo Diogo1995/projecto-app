290de7afb6299debcf6531441288f0ddf073cf60
Catarina Pinhão <37343617+catpinhao@users.noreply.github.com>
2018-03-22 13:52:11 +0000

    settings v3
    radio buttons criados
    dropdown de selecção de idioma criado
    falta aplicar a lógica de todas as escolhas possíveis

--- Barraqueiro/app/src/main/java/com/example/diogo/barraqueiro/Settings.java
+++ Barraqueiro/app/src/main/java/com/example/diogo/barraqueiro/Settings.java
@@ -1,12 +1,15 @@
 package com.example.diogo.barraqueiro;
 
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
     @Override
@@ -16,10 +19,50 @@ public class Settings extends AppCompatActivity {
 
         // Ver o estado da switch
         Switch darkModeSwitch = (Switch) findViewById(R.id.darkModeSwitch);
-        Boolean darkModeSwitchState = darkModeSwitch.isChecked();
-        // TODO alterar cores
+        if(darkModeSwitch.isChecked()){
+            // TODO alterar cores
+        }
 
-        // TODO acrescentar  restantes botões e coisas - https://stackoverflow.com/questions/13377361/how-to-create-a-drop-down-list
+        Spinner languages = findViewById(R.id.languageDropDown);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.language_entries, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languages.setAdapter(adapter);
+        languages.setOnItemSelectedListener(this);
+    }
 
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // TODO idioma escolhido
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO mostrar idioma default (está a mostrar o o primeiro idioma da lista agora)
+    }
+
+    public void distanceRadioButtonClicked(View v){
+        boolean checked = ((RadioButton) v).isChecked();
+        switch(v.getId()) {
+            case R.id.kmButton:
+                if (checked)
+                    // TODO distância em km
+                break;
+            case R.id.mileButton:
+                if (checked)
+                    // TODO distância em milhas
+                break;
+        }
+    }
+
+    public void volumeRadioButtonClicked(View v){
+        boolean checked = ((RadioButton) v).isChecked();
+        switch(v.getId()) {
+            case R.id.lButton:
+                if (checked)
+                    // TODO volume em litros
+                    break;
+            case R.id.galButton:
+                if (checked)
+                    // TODO volume em gallons
+                    break;
+        }
     }
 }
