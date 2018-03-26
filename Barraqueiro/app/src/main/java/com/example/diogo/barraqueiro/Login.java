5405efdfc31db0b82a7306f556b755499bf086f6
Catarina Pinhão <37343617+catpinhao@users.noreply.github.com>
2018-03-26 10:40:59 +0100

    icons v3
    acrescentei imagens a branco para o menu em dark mode (ainda não implementei porque estava a dar erro)
    limpei o código do login

--- Barraqueiro/app/src/main/java/com/example/diogo/barraqueiro/Login.java
+++ Barraqueiro/app/src/main/java/com/example/diogo/barraqueiro/Login.java
@@ -32,9 +32,7 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-public class Login extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-    private static final int REQUEST_READ_CONTACTS = 0;
-
+public class Login extends AppCompatActivity  {
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo:hello", "bar:world"
     };
@@ -77,8 +75,6 @@ public class Login extends AppCompatActivity implements LoaderManager.LoaderCall
 
         // Set up the login form.
         usernameView = (AutoCompleteTextView) findViewById(R.id.username);
-        populateAutoComplete();
-
         passwordView = (EditText) findViewById(R.id.password);
         passwordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -91,50 +87,13 @@ public class Login extends AppCompatActivity implements LoaderManager.LoaderCall
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.loginButton);
-        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+        Button signInButton = (Button) findViewById(R.id.loginButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
         });
-    }
-
-    private void populateAutoComplete() {
-        if (!mayRequestContacts())
-            return;
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-            return true;
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED)
-            return true;
-
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS))
-            Snackbar.make(usernameView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        else
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-
-        return false;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
     }
 
     private void attemptLogin() {
@@ -208,60 +167,6 @@ public class Login extends AppCompatActivity implements LoaderManager.LoaderCall
         return password.length() > 4;
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> usernames = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            usernames.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(usernames);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> usernameCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(Login.this,
-                        android.R.layout.simple_dropdown_item_1line, usernameCollection);
-
-        usernameView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
         private final String mUsername;
         private final String mPassword;
@@ -274,7 +179,6 @@ public class Login extends AppCompatActivity implements LoaderManager.LoaderCall
         @Override
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
-
             try {
                 // Simulate network access.
                 Thread.sleep(2000);
@@ -285,12 +189,9 @@ public class Login extends AppCompatActivity implements LoaderManager.LoaderCall
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mUsername)) {
-                    // Account exists, return true if the password matches.
                     return pieces[1].equals(mPassword);
                 }
             }
-
-            // TODO: register the new account here.
             return true;
         }
 
@@ -298,9 +199,7 @@ public class Login extends AppCompatActivity implements LoaderManager.LoaderCall
         protected void onPostExecute(final Boolean success) {
             mAuthTask = null;
 
-            if (success) {
-                //finish();
-            } else {
+            if(!success){
                 passwordView.setError(getString(R.string.error_incorrect_password));
                 passwordView.requestFocus();
             }
